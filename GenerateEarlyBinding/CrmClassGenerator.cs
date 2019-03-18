580d1f80ab6049964cc883e3e882fe2329d707e2
bhargav <bharg@DESKTOP-52PA0BS>
2019-03-18 09:00:00 +0530

    Add project files.

---
+++ GenerateEarlyBinding/CrmClassGenerator.cs
@@ -0,0 +1,137 @@
+﻿using CodeWriter;
+using Microsoft.Xrm.Sdk.Metadata;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigLynx
+{
+    public class CrmClassGenerator
+    {
+        public void GenerateClassFile(EntityMetadataCollection entityMetaDataCollection)
+        {
+            var w = new CodeWriter.CodeWriter(CodeWriterSettings.CSharpDefault);
+
+            w._("using System;");
+            w._("using System.Collections.Generic;");
+            w._("using System.Linq;");
+            w._("using System.Text;");
+            w._("using System.Threading.Tasks;");
+            w._("using Microsoft.Xrm.Sdk;");
+            w._("");
+
+            using (w.B("namespace Xrm"))
+            {
+                foreach (EntityMetadata entityMetaData in entityMetaDataCollection)
+                {
+
+                    using (w.B("public class " + entityMetaData.LogicalName + ": Entity"))
+                    {
+                        var attributes = entityMetaData.Attributes.Where(filter => filter.AttributeType.ToString() != "Virtual").ToList();
+                        foreach (var att in attributes)
+                        {
+                            w._("public " + GetAttributeTypeName(att.AttributeType.ToString()) + " " + att.LogicalName + "{ get; set;}");
+                        }
+                        using (w.B("public Entity ToEntity()"))
+                        {
+                            w._("Entity ent = new Entity(this.GetType().Name);");
+
+                            using (w.B("try"))
+                            {
+                                using (w.B("foreach (var att in this.GetType().GetProperties())"))
+                                {
+                                    using (w.B("if (att.Name != \"KeyAttributes\" && att.Name != \"Item\" && att.Name  != \"ExtensionData\" && att.Name != \"entityimageid\" && att.Name != \"Attributes\" && att.Name != \"FormattedValues\" && att.Name != \"RelatedEntities\")"))
+                                    {
+                                        using (w.B("if (att.GetValue(this) != null)"))
+                                        {
+                                            using (w.B("if (att.GetValue(this).GetType().Name != \"Guid\")"))
+                                            {
+                                                w._("ent[att.Name] = att.GetValue(this);");
+                                            }
+                                            using (w.B("else if(att.GetValue(this).GetType().Name == \"Guid\" && (Guid)att.GetValue(this) != Guid.Empty)"))
+                                            {
+                                                w._("ent[att.Name] = att.GetValue(this);");
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            using (w.B("catch (Exception ex)"))
+                            {
+                                w._("throw new Exception(ex.Message);");
+                            }
+                        }
+                    }
+                }
+            }
+            string executableLocation = Path.GetDirectoryName(
+    System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string csLocation = Path.Combine(executableLocation, "Xrm.cs");
+            File.WriteAllText(csLocation, w.ToString());
+            Console.WriteLine(csLocation);
+        }
+        private string GetAttributeTypeName(string attributeType)
+        {
+
+            string returnString = string.Empty;
+            switch (attributeType)
+            {
+                case "BigInt":
+                    returnString = "Int64";
+                    break;
+                case "String":
+                    returnString = "String";
+                    break;
+                case "DateTime":
+                    returnString = "System.Nullable<DateTime>";
+                    break;
+                case "Double":
+                    returnString = "System.Nullable<double>";
+                    break;
+                case "Decimal":
+                    returnString = "System.Nullable<decimal>";
+                    break;
+                case "Boolean":
+                    returnString = "System.Nullable<Boolean>";
+                    break;
+                case "Status":
+                    returnString = "OptionSetValue";
+                    break;
+                case "State":
+                    returnString = "OptionSetValue";
+                    break;
+                case "Integer":
+                    returnString = "System.Nullable<int>";
+                    break;
+                case "Memo":
+                    returnString = "String";
+                    break;
+                case "Lookup":
+                    returnString = "EntityReference";
+                    break;
+                case "EntityName":
+                    returnString = "String";
+                    break;
+                case "Customer":
+                    returnString = "EntityReference";
+                    break;
+                case "Uniqueidentifier":
+                    returnString = "Guid";
+                    break;
+                case "Money":
+                    returnString = "Microsoft.Xrm.Sdk.Money";
+                    break;
+                case "Picklist":
+                    returnString = "Microsoft.Xrm.Sdk.OptionSetValueCollection";
+                    break;
+                case "Owner":
+                    returnString = "EntityReference";
+                    break;
+            }
+            return returnString;
+        }
+    }
+}
---
+++ GenerateEarlyBinding/Program.cs
@@ -0,0 +1,127 @@
+﻿using BigLynx;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace GenerateEarlyBinding
+{
+    class Program
+    {
+        private static CrmServiceClient _client;
+        private static EntityCollection solutionsList;
+        private static int solutionNumber;
+        private static Entity currentSolution;
+        public static EntityCollection SolutionsList { get => solutionsList; set => solutionsList = value; }
+        public static int SolutionNumber { get => solutionNumber; set => solutionNumber = value; }
+        public static Entity CurrentSolution { get => currentSolution; set => currentSolution = value; }
+        public static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length > 0)
+                {//ConfigurationManager.ConnectionStrings["CRMConnectionString"].ConnectionString
+                    string connectionString = "";
+                    foreach (string str in args)
+                        connectionString += str;
+                    try
+                    {
+                        using (_client = new CrmServiceClient(connectionString))
+                        {
+                            //Do stuff
+                            Console.WriteLine("Connection Established Successfully...");
+                            SolutionsList = new EntityCollection();
+                            SolutionNumber = 0;
+                            CurrentSolution = new Entity();
+
+                            RetrieveCRMSolutions(_client);
+
+                            var listOfEntitiesMetadata = RetrieveSolutionRelatedEntities(_client);
+
+
+                            CrmClassGenerator crmClassGenerator = new CrmClassGenerator();
+                            EntityMetadataCollection coll = new EntityMetadataCollection();
+                            foreach (EntityMetadata meta in listOfEntitiesMetadata)
+                                coll.Add(meta);
+                            crmClassGenerator.GenerateClassFile(coll);
+
+                            Console.WriteLine("File is located @ above location");
+                        }
+                    }
+                    catch (Exception ex) {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+            catch (FaultException<OrganizationServiceFault> ex)
+            {
+                string message = ex.Message;
+                throw;
+            }
+        }
+        private static IEnumerable<EntityMetadata> RetrieveSolutionRelatedEntities(IOrganizationService organizationService)
+        {
+            QueryExpression componentsQuery = new QueryExpression
+            {
+                EntityName = "solutioncomponent",
+                ColumnSet = new ColumnSet("objectid"),
+                Criteria = new FilterExpression(),
+            };
+            LinkEntity solutionLink = new LinkEntity("solutioncomponent", "solution", "solutionid", "solutionid", JoinOperator.Inner);
+            solutionLink.LinkCriteria = new FilterExpression();
+            solutionLink.LinkCriteria.AddCondition(new ConditionExpression("solutionid", ConditionOperator.Equal, CurrentSolution.Id));
+            componentsQuery.LinkEntities.Add(solutionLink);
+            componentsQuery.Criteria.AddCondition(new ConditionExpression("componenttype", ConditionOperator.Equal, 1));
+            EntityCollection ComponentsResult = organizationService.RetrieveMultiple(componentsQuery);
+            //Get all entities
+            RetrieveAllEntitiesRequest AllEntitiesrequest = new RetrieveAllEntitiesRequest()
+            {
+                EntityFilters = EntityFilters.Entity | Microsoft.Xrm.Sdk.Metadata.EntityFilters.Attributes,
+                RetrieveAsIfPublished = true
+            };
+            RetrieveAllEntitiesResponse AllEntitiesresponse = (RetrieveAllEntitiesResponse)organizationService.Execute(AllEntitiesrequest);
+            return AllEntitiesresponse.EntityMetadata.Join(ComponentsResult.Entities.Select(x => x.Attributes["objectid"]), x => x.MetadataId, y => y, (x, y) => x);
+        }
+
+        private static void RetrieveCRMSolutions(IOrganizationService organizationService)
+        {
+            QueryExpression query = new QueryExpression()
+            {
+                EntityName = "solution",
+                ColumnSet = new ColumnSet(true),
+            };
+            SolutionsList = organizationService.RetrieveMultiple(query);
+            if (SolutionsList.Entities.Count > 0)
+            {
+                for (var i = 0; i < SolutionsList.Entities.Count; i++)
+                {
+                    Console.WriteLine(i + 1 + ". " + SolutionsList.Entities[i]["friendlyname"] + " (" + SolutionsList.Entities[i]["uniquename"] + ")");
+                }
+                Console.WriteLine("");
+                Console.WriteLine("Please enter solution # and press 'Enter' key");
+                var key = Console.ReadLine();
+                int number;
+                if (Int32.TryParse(key, out number))
+                {
+                    SolutionNumber = number;
+                    if (SolutionsList.Entities.Count >= number - 1)
+                        CurrentSolution = SolutionsList.Entities[number - 1];
+                    else
+                        Console.WriteLine("Enter a valid Number");
+                }
+                else
+                {
+                    Console.WriteLine("Enter a valid Number");
+                }
+            }
+        }
+    }
+}
